9b3652b7e9080e49de08aca5609b0e5685815ad4
msaf1980 <msaf1980@gmail.com>
2023-01-13 17:14:37 +0500

    add time value from string

--- time.go
+++ time.go
@@ -17,6 +17,18 @@ func newTimeValue(val time.Time, p *time.Time, layout string) *timeValue {
 	return tv
 }
 
+func newTimeValueFromString(val string, p *time.Time, layout string) *timeValue {
+	v, err := time.Parse(layout, val)
+	if err == nil {
+		panic(err)
+	}
+	tv := new(timeValue)
+	tv.layout = layout
+	tv.value = p
+	*tv.value = v
+	return tv
+}
+
 func (t *timeValue) Set(s string, _ bool) error {
 	v, err := time.Parse(t.layout, s)
 	if err == nil {
@@ -49,5 +61,15 @@ func (t *timeValue) GetTime() time.Time { return *t.value }
 // registered `*Opt` object returned.
 func (commandConfig *CommandConfig) AddTime(name, shortName string, value time.Time, p *time.Time, layout string, help string) *Opt {
 	v := newTimeValue(value, p, layout)
+	return commandConfig.AddValue(name, shortName, v, help)
+}
+
+// AddTimeFromString registers an duration argument configuration with the command.
+// The `name` argument represents the name of the argument.
+// The `shortName` argument represents the short alias of the argument.
+// If an argument with given `name` is already registered, then panic
+// registered `*Opt` object returned.
+func (commandConfig *CommandConfig) AddTimeFromString(name, shortName string, value string, p *time.Time, layout string, help string) *Opt {
+	v := newTimeValueFromString(value, p, layout)
 	return commandConfig.AddValue(name, shortName, v, help)
 }
